fad6c863cce1e8f6956edda3ebdb2344958b2449
Nikita <npost.al@yandex.ru>
2023-05-22 22:22:15 +0300

    ex15

--- ex_15.py
+++ ex_15.py
@@ -10,3 +10,15 @@
 # число – это масса соответствующего арбуза
 
 
+watermelons = int(input("Введите кол-во арбузов: "))
+
+max_kg = int(input("Введите массу арбуза: "))
+min_kg = max_kg
+for _ in range(watermelons - 1):
+    weight = int(input("Введите массу арбуза: "))
+    if weight > max_kg:
+        max_kg = weight
+    else:
+        if weight < min_kg:
+            min_kg = weight
+print(f"Для себя любимого - {max_kg}, для любимой тещи - {min_kg}")
